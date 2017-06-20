fd98e923fe8e06f6ac55c0ee8d20417e2e372e52
Chad Engler <englercj@live.com>
2017-06-20 09:03:19 -0700

    Fix #4, preserve context when calling `oldXHR`.

--- js/jquery.ajax-progress.js
+++ js/jquery.ajax-progress.js
@@ -10,7 +10,7 @@
     //patch ajax settings to call a progress callback
     var oldXHR = $.ajaxSettings.xhr;
     $.ajaxSettings.xhr = function() {
-        var xhr = oldXHR();
+        var xhr = oldXHR.apply(this, arguments);
         if(xhr instanceof window.XMLHttpRequest) {
             xhr.addEventListener('progress', this.progress, false);
         }
